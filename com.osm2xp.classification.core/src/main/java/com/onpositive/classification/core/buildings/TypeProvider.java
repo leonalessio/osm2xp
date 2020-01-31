ad701ddabc5065fe9e1a5a1772db91cb5a16553f
Dmitry Karpenko <dmitry@onpositive.com>
2020-01-31 19:39:02 +0700

    Improve the type getting for 3D objects a little bit
    Change-Id: I45badf802acf7251d4cbb1a8b127fc166b159985

--- com.osm2xp.classification.core/src/main/java/com/onpositive/classification/core/buildings/TypeProvider.java
+++ com.osm2xp.classification.core/src/main/java/com/onpositive/classification/core/buildings/TypeProvider.java
@@ -9,11 +9,34 @@ import com.osm2xp.core.model.osm.Tag;
 
 public class TypeProvider {
 	public static OSMBuildingType getBuildingType(List<Tag> tags) {
-		String building = TagUtil.getValue("building", tags);
 		String shop = TagUtil.getValue("shop", tags);
 		if ("department_store".equals(shop) || "mall".equals(shop)) {
 			return OSMBuildingType.MALL;
 		}
+		String building = TagUtil.getValue("building", tags);
+		OSMBuildingType type = getFromBuildingTag(tags, building);
+		if (type == null) {
+			String buildingUse = TagUtil.getValue("building:use", tags);
+			type = getFromBuildingTag(tags, buildingUse);
+		}
+		String amenity = TagUtil.getValue("amenity", tags);
+		if (amenity != null && amenity.endsWith("school") || "college".equals(amenity)) {
+			return OSMBuildingType.SOCIAL;
+		}
+		if ("university".equals(amenity)) {
+			return OSMBuildingType.SOCIAL;
+		}
+		if ("clinic".equals(amenity) || "hospital".equals(amenity) || 
+			"dentist".equals(amenity) || "doctors".equals(amenity)) {
+			return OSMBuildingType.SOCIAL;
+		}
+		if (!StringUtils.isEmpty(shop)) {
+			return OSMBuildingType.SHOP;
+		}		
+		return type;
+	}
+
+	protected static OSMBuildingType getFromBuildingTag(List<Tag> tags, String building) {
 		if ("hospital".equals(building)) {
 			return OSMBuildingType.SOCIAL;
 		}
@@ -23,15 +46,7 @@ public class TypeProvider {
 		if ("retail".equals(building) || "supermarket".equals(building)) {
 			return OSMBuildingType.SHOP;
 		}
-		String amenity = TagUtil.getValue("amenity", tags);
-		if (amenity != null && amenity.endsWith("school") || "college".equals(amenity)) {
-			return OSMBuildingType.SOCIAL;
-		}
-		if ("university".equals(building) || "university".equals(amenity)) {
-			return OSMBuildingType.SOCIAL;
-		}
-		if ("clinic".equals(amenity) || "hospital".equals(amenity) || 
-			"dentist".equals(amenity) || "doctors".equals(amenity)) {
+		if ("university".equals(building)) {
 			return OSMBuildingType.SOCIAL;
 		}
 		if (building != null && building.startsWith("garage")) {
@@ -46,7 +61,7 @@ public class TypeProvider {
 		if ("house".equals(building) || "detached".equals(building)) {
 			return OSMBuildingType.HOUSE;
 		}
-		if ("commercial".equals(building) || !StringUtils.isEmpty(shop)) {
+		if ("commercial".equals(building)) {
 			return OSMBuildingType.SHOP;
 		}
 		return null;
